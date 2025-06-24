bcdf413b1d06081aacceac6899f42c2199352cb6
gaurav-jain000 <116086566+gaurav-jain000@users.noreply.github.com>
2025-06-24 23:00:07 +0530

    Wired up with Shadcn UI and tailwind typography plugin

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,10 +1,11 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Showcasy" },
+    { name: "description", content: "Welcome to Showcasy!" },
   ];
 }
 
@@ -13,5 +14,21 @@ export function loader({ context }: Route.LoaderArgs) {
 }
 
 export default function Home({ loaderData }: Route.ComponentProps) {
-  return <Welcome message={loaderData.message} />;
+  return (
+    <>
+      <section className="min-h-[100vh] grid place-items-center bg-gradient-to-br from-stone-800 via-stone-700 to-stone-950">
+        <div className=" border rounded-xl bg-gray-100/10 p-8 border-gray-100/20 w-1/4">
+          <div className="prose prose-lg max-w-full prose-headings:my-3 text-center">
+            <h1 className="text-gray-200">Showcasy</h1>
+            <p className="text-gray-200 text-base">
+              Welcome to showcasy! <br /> We are glad to have you here.
+            </p>
+          </div>
+          <div className="mt-6 w-fit mx-auto">
+            <Button>Let's get started!</Button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
